70ee492cd0142bb3fddf0c74a122c4b9d1f54ccc
Laiara13 <laiara-leizer@educar.rs.gov.br>
2024-07-05 14:24:44 -0300

    versao 8

--- src/api.js
+++ src/api.js
@@ -18,7 +18,17 @@ module.exports=app;
 
 
 
+require("dotenv").config(); 
+const app = require("../src/api");
 
+app.use((req,res, next) => {
+next();
+});
+                     //API_PORT
+let port = process.env.API_PORT || 5000; //mudar pra minha
 
+app.listen(port);
+
+console.log("Iniciando na porta " + port);
 
 
