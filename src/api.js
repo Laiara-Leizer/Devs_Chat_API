858e8bc6d5a65dbf0762caf5104269bd7cb63c39
Laiara-Leizer <laiaraleizer06@gmail.com>
2024-12-01 20:21:13 -0300

    tentativa de resolver o problema da rota /entrar v2

--- src/api.js
+++ src/api.js
@@ -1,3 +1,92 @@
+// // const express = require("express");
+// // var app = express();
+// // app.use(express.urlencoded({extended : true}));
+// // app.use(express.json());
+
+// // const router = express.Router();
+
+// // app.use('/', router.get('/', (req, res)=>{
+// //     res.status(200).send("<h1>API - CHAT</h1>")
+// // }))
+
+// // //module export: exporta o objeto que quero usar em outro lugar
+
+// // app.use("/",router.get("/",(req,res, next) => {
+// //     res.status(200).send("<h1>API - CHAT</h1>");
+// //     }));
+    
+// //     app.use("/",router.get("/sobre", (req, res, next) => {
+// //     res.status(200) .send({
+// //     "nome": "API - CHAT",
+// //     "versão": "1.0.0",
+// //     "autor": "Laiara Leizer"
+// //     })
+// // }));
+
+// // // await async
+
+// // const salaController = require("./controllers/SalaController.js");
+// // const token = require("./util/token.js");
+// // const { listarSalas } = require("./models/SalaModel.js");
+
+// //     app.use("/salas",router.get("/salas", async (req, res,next) => {
+// //       if(await token.checktoken(req.headers.token,req.headers.iduser,req.headers.nick)) {
+// //         let resp = await salaController.get();
+// //         res.status(200).send(resp);
+// //       }else{
+// //         res.status(400).send({msg:"Usuário não autorizado"});
+// //       }
+
+// //     }));
+    
+    
+// //     app.use("/entrar",router.post("/entrar", async(req, res, next) => {
+// //       const usuarioController = require("./controllers/usuarioController");   
+// //       let resp= await usuarioController.entrar(req.body.nick);
+// //       res.status(200).send(resp);
+// //   }));
+
+// //   app.use('/sala/entrar', router.put('/sala/entrar', async (req, res)=>{
+// //     if(!token.checktoken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
+// //     let resp= await salaController.entrar(req.headers.iduser, req.query.idsala);
+// //     res.status(200).send(resp);
+// //   }))
+
+
+// //   app.use("/sala/mensagem/", router.post("/sala/mensagem", async (req, res) => {
+// //     if(!token.checktoken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
+// //     let resp = await salaController.enviarMensagem(req.headers.nick, req.body.msg,req.body.idsala);
+// //     res.status(200).send(resp);
+// //   }));
+  
+
+// //   app.use("/sala/mensagens/", router.get("/sala/mensagens", async (req, res) => {
+// //     if(!token.checktoken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
+// //     let resp= await salaController.buscarMensagens(req.query.idsala, req.query.timestamp);
+// //     res.status(200).send(resp);
+// //   }));
+// //   //aqui
+  
+
+// //     module.exports = app;
+
+
+
+// //     // console.log(listarSalas);
+// //     // console.log(token);
+// //     // console.log(salaController);
+
+
+
+
+
+
+
+
+
+
+
+
 // const express = require("express");
 // var app = express();
 // app.use(express.urlencoded({extended : true}));
@@ -87,103 +176,89 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const express = require("express");
-var app = express();
+const app = express();
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
 const router = express.Router();
 
-app.use('/', router.get('/', (req, res)=>{
+// Configuração do servidor
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
+
+app.get('/', (req, res) => {
     res.status(200).send("<h1>API - CHAT</h1>")
-}))
+});
 
-//module export: exporta o objeto que quero usar em outro lugar
-
-app.use("/",router.get("/",(req,res, next) => {
-    res.status(200).send("<h1>API - CHAT</h1>");
-    }));
-    
-    app.use("/",router.get("/sobre", (req, res, next) => {
-    res.status(200) .send({
-    "nome": "API - CHAT",
-    "versão": "1.0.0",
-    "autor": "Laiara Leizer"
+app.get("/sobre", (req, res) => {
+    res.status(200).send({
+        "nome": "API - CHAT",
+        "versão": "1.0.0",
+        "autor": "Laiara Leizer"
     })
-}));
-
-// await async
+});
 
 const salaController = require("./controllers/SalaController.js");
 const token = require("./util/token.js");
 const { listarSalas } = require("./models/SalaModel.js");
 
-    app.use("/salas",router.get("/salas", async (req, res,next) => {
-      if(await token.checktoken(req.headers.token,req.headers.iduser,req.headers.nick)) {
+app.get("/salas", async (req, res) => {
+    if(await token.checktoken(req.headers.token, req.headers.iduser, req.headers.nick)) {
         let resp = await salaController.get();
         res.status(200).send(resp);
-      }else{
+    } else {
         res.status(400).send({msg:"Usuário não autorizado"});
-      }
+    }
+});
 
-    }));
-    
-    
-    app.use("/entrar",router.post("/entrar", async(req, res, next) => {
-      const usuarioController = require("./controllers/usuarioController");   
-      let resp= await usuarioController.entrar(req.body.nick);
-      res.status(200).send(resp);
-  }));
-
-
-
-
-
-
-
-  
-
-  // app.use('/sala/entrar', router.put('/sala/entrar', async (req, res)=>{
-  //   if(!token.checktoken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
-  //   let resp= await salaController.entrar(req.headers.iduser, req.query.idsala);
-  //   res.status(200).send(resp);
-  // }));
-
-  app.use("/entrar",router.post("/entrar", async(req, res, next) => {
+app.post("/entrar", async(req, res) => {
     const usuarioController = require("./controllers/usuarioController");   
-    let resp= await usuarioController.entrar(req.body.nick);
+    let resp = await usuarioController.entrar(req.body.nick);
     res.status(200).send(resp);
-  }));
+});
 
-
-
-
-
-
-
-
-
-
-
-  app.use("/sala/mensagem/", router.post("/sala/mensagem", async (req, res) => {
-    if(!token.checktoken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
-    let resp = await salaController.enviarMensagem(req.headers.nick, req.body.msg,req.body.idsala);
+app.put('/sala/entrar', async (req, res) => {
+    if(!token.checktoken(req.headers.token, req.headers.iduser, req.headers.nick)) return false;
+    let resp = await salaController.entrar(req.headers.iduser, req.query.idsala);
     res.status(200).send(resp);
-  }));
-  
+});
 
-  app.use("/sala/mensagens/", router.get("/sala/mensagens", async (req, res) => {
-    if(!token.checktoken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
-    let resp= await salaController.buscarMensagens(req.query.idsala, req.query.timestamp);
+app.post("/sala/mensagem", async (req, res) => {
+    if(!token.checktoken(req.headers.token, req.headers.iduser, req.headers.nick)) return false;
+    let resp = await salaController.enviarMensagem(req.headers.nick, req.body.msg, req.body.idsala);
     res.status(200).send(resp);
-  }));
-  //aqui
-  
+});
 
-    module.exports = app;
+app.get("/sala/mensagens", async (req, res) => {
+    if(!token.checktoken(req.headers.token, req.headers.iduser, req.headers.nick)) return false;
+    let resp = await salaController.buscarMensagens(req.query.idsala, req.query.timestamp);
+    res.status(200).send(resp);
+});
 
+app.listen(PORT, HOST, () => {
+    console.log(`Server running on http://${HOST}:${PORT}`);
+});
 
-
-    // console.log(listarSalas);
-    // console.log(token);
-    // console.log(salaController);
+module.exports = app;
