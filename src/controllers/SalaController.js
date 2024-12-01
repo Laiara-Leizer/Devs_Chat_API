c875620268bb67c960f0c47ad682dc05c8377e83
Laiara-Leizer <laiaraleizer06@gmail.com>
2024-12-01 03:49:51 -0300

    VF Versão Final-2

--- src/controllers/SalaController.js
+++ src/controllers/SalaController.js
@@ -1,3 +1,64 @@
+// exports.get = async (req, res) => {
+
+//   const salaModel = require('../models/SalaModel');
+//     return await salaModel.listarSalas();
+//   }
+
+//   exports.entrar = async (iduser, idsala) => {
+//     const salaModel = require("../models/SalaModel");
+//     const sala = await salaModel.buscarSala(idsala);
+//     const usuarioModel = require("../models/usuarioModel");
+//     const user = await usuarioModel.buscarUsuario(iduser);
+
+//     if (!user) {
+//       console.error("Usuário não encontrado.");
+//       return false;
+//     }
+//     user.sala = { _id: sala._id, nome: sala.nome, tipo: sala.tipo };
+
+//     if (await usuarioModel.alterarUsuario(user)) {
+//       return { msg: "OK", timestamp: Date.now() };
+//     } else {
+//       console.error("Erro ao atualizar o usuário.");
+//       return false;
+//     }
+//   };
+
+// exports.enviarMensagem= async (nick, msg, idsala)=>{
+//   const salaModel = require('../models/SalaModel');
+// 	const sala = await salaModel.buscarSala(idsala);
+//   console.log(msg);
+
+//   if (!sala) {
+//     console.error("Sala não encontrada.");
+//     return { msg: "Erro: Sala não encontrada." };
+// }
+// 		if(!sala.msgs){
+// 		sala.msgs=[""];
+// 	}
+// 	timestamp=Date.now()
+// 	sala.msgs.push(
+// 		{
+// 			timestamp:timestamp,
+// 			msg:msg,
+// 			nick:nick
+// 		}
+// 	)
+// 	let resp = await salaModel.atualizarMensagens(sala);
+// 	return {"msg":"OK", "timestamp":timestamp};
+// }
+
+// exports.buscarMensagens = async (idsala, timestamp)=>{
+//   const salaModel = require('../models/SalaModel');
+//   let mensagens=await salaModel.buscarMensagens(idsala, timestamp);
+//   return {
+//     "timestamp":mensagens[mensagens.length - 1].timestamp,
+//     "msgs":mensagens
+//   };
+// }  
+// exports = {};
+
+
 exports.get = async (req, res) => {
 
   const salaModel = require('../models/SalaModel');
--- src/models/SalaModel.js
+++ src/models/SalaModel.js
@@ -1,3 +1,47 @@
+// const db = require("./db");
+
+// let listarSalas = async () => {
+//     // console.log(listarSalas);
+//     let salas = await db.findAll("salas");
+//     // console.log(salas);
+//     return salas;
+// };
+
+// let buscarSala = async (idsala)=>{
+//   // console.log(buscarSala);
+//     return db.findOne("salas",idsala);
+//   }
+
+//   let atualizarMensagens=async (sala)=>{
+//     return await db.updateOne("salas", sala,{_id:sala._id});
+//   }
+
+//   let buscarMensagens = async (idsala, timestamp)=>{
+//     let sala = await buscarSala(idsala);
+
+//     if(!sala){
+//       console.error("sala nao encontrada.");
+//     }
+    
+//     if(sala.msgs){
+//       let msgs=[];
+//       sala.msgs.forEach((msg)=>{
+//         if(msg.timestamp >= timestamp){
+//           msgs.push(msg);
+//         }
+//       });
+//       return msgs;
+//     }
+//     return [];
+// }
+
+// //aqui
+
+// // console.log(listarSalas);
+// // Aparece
+
+// module.exports = { listarSalas, buscarSala, atualizarMensagens, buscarMensagens };
+
 const db = require("./db");
 
 let listarSalas = async () => {
