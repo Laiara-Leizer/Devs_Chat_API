aa7be894adaf711383f4dccd467df9a00f013a90
Laiara-Leizer <laiaraleizer06@gmail.com>
2024-12-01 04:03:09 -0300

    VF Versão Final-3

--- src/models/SalaModel.js
+++ src/models/SalaModel.js
@@ -42,46 +42,64 @@
 
 // module.exports = { listarSalas, buscarSala, atualizarMensagens, buscarMensagens };
 
-const db = require("./db");
+// const db = require("./db");
 
-let listarSalas = async () => {
-    // console.log(listarSalas);
-    let salas = await db.findAll("salas");
-    // console.log(salas);
-    return salas;
+// const listarSalas = async () => {
+//   const salas = await findAll('salas');
+//   return salas;
+// };
+
+
+
+
+
+
+// let listarSalas = async () => {
+//     // console.log(listarSalas);
+//     let salas = await db.findAll("salas");
+//     // console.log(salas);
+//     return salas;
+// };
+
+
+
+
+
+
+// Importando a função findAll corretamente
+const { findAll, findOne, updateOne } = require("./db");
+
+const listarSalas = async () => {
+  // Agora a função findAll está sendo chamada corretamente
+  const salas = await findAll('salas');
+  return salas;
 };
 
-let buscarSala = async (idsala)=>{
-  // console.log(buscarSala);
-    return db.findOne("salas",idsala);
+let buscarSala = async (idsala) => {
+  return findOne("salas", idsala);
+};
+
+let atualizarMensagens = async (sala) => {
+  return await updateOne("salas", sala, { _id: sala._id });
+};
+
+let buscarMensagens = async (idsala, timestamp) => {
+  let sala = await buscarSala(idsala);
+
+  if (!sala) {
+    console.error("Sala não encontrada.");
   }
 
-  let atualizarMensagens=async (sala)=>{
-    return await db.updateOne("salas", sala,{_id:sala._id});
+  if (sala.msgs) {
+    let msgs = [];
+    sala.msgs.forEach((msg) => {
+      if (msg.timestamp >= timestamp) {
+        msgs.push(msg);
+      }
+    });
+    return msgs;
   }
-
-  let buscarMensagens = async (idsala, timestamp)=>{
-    let sala = await buscarSala(idsala);
-
-    if(!sala){
-      console.error("sala nao encontrada.");
-    }
-    
-    if(sala.msgs){
-      let msgs=[];
-      sala.msgs.forEach((msg)=>{
-        if(msg.timestamp >= timestamp){
-          msgs.push(msg);
-        }
-      });
-      return msgs;
-    }
-    return [];
-}
-
-//aqui
-
-// console.log(listarSalas);
-// Aparece
+  return [];
+};
 
 module.exports = { listarSalas, buscarSala, atualizarMensagens, buscarMensagens };
--- src/models/db.js
+++ src/models/db.js
@@ -55,51 +55,38 @@
 // jwt(id,key,time)
 //key é o nick do usuario
 
-const { MongoClient, ObjectId, Collection } = require("mongodb");
-const { listarSalas } = require("./SalaModel");
+const { MongoClient, ObjectId } = require("mongodb");
 let singleton;
 
 async function connect() {
- 
-if (singleton) return singleton;
-// console.log(process.env.DB_HOST);
-
-
-const client = new MongoClient(process.env.DB_HOST);
-await client.connect();
-// console.log(process.env.DB);
-singleton = client.db(process.env.DB);
-return singleton;
+  if (singleton) return singleton;
+  const client = new MongoClient(process.env.DB_HOST);
+  await client.connect();
+  singleton = client.db(process.env.DB);
+  return singleton;
 }
 
-let findAll = async (collection)=>{
-    const db = await connect();
-    // console.log(findAll);
-    return await db.collection(collection).find().toArray();
-  }
-
-async function insertOne(collection, objeto){
-    const db = await connect();
-    // console.log(insertOne);
-    //insertOne
-    return db.collection(collection).insertOne(objeto);
+let findAll = async (collection) => {
+  const db = await connect();
+  return await db.collection(collection).find().toArray();
 }
 
-  //to pasando Objectid
-  let findOne = async (collection, _id)=>{
-    const db = await connect();
-    let obj= await db.collection(collection).find({'_id':new ObjectId(_id)}).toArray();
-    if(obj)
-      return obj[0];
-    return false;
-  }
-  
-  let updateOne= async (collection, object, param)=>{
-    const db = await connect();
-    let result= await db.collection(collection).updateOne(param, { $set: object} );
-    return result;
-  }
+let insertOne = async (collection, objeto) => {
+  const db = await connect();
+  return db.collection(collection).insertOne(objeto);
+}
 
-  console.log(Collection);
-  
-module.exports = {findAll, insertOne, findOne, updateOne}
+let findOne = async (collection, _id) => {
+  const db = await connect();
+  let obj = await db.collection(collection).find({ '_id': new ObjectId(_id) }).toArray();
+  if (obj) return obj[0];
+  return false;
+}
+
+let updateOne = async (collection, object, param) => {
+  const db = await connect();
+  let result = await db.collection(collection).updateOne(param, { $set: object });
+  return result;
+}
+
+module.exports = { findAll, insertOne, findOne, updateOne };
